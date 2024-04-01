dc97f1c59b6d04d748535c0c251c53c5bee53722
derek <derektang101@gmail.com>
2024-03-31 21:55:25 -0400

    added undo history, git plugins, statusbar, and configured starship for zsh

--- .config/nvim/lua/derek/core/options.lua
+++ .config/nvim/lua/derek/core/options.lua
@@ -13,6 +13,12 @@ opt.autoindent = true -- copy indent from current line when starting new one
 -- line wrapping
 opt.wrap = false -- disable line wrapping
 
+--swaps and backups
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
